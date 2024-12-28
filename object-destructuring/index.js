e2262de60165c2ec6bb98fa40a9f3294cda3a5c2
athulsabu <athulsabu75@gmail.com>
2024-12-28 10:28:14 +0530

    update

---
+++ object-destructuring/index.js
@@ -0,0 +1,25 @@
+const dreamHoliday = {
+    destination: 'Switzerland',
+    activity: 'Skiing in the Alps and exploring scenic trails',
+    accommodation: 'Luxury chalet with mountain views',
+    companion: 'Family',
+    duration: '10 days',
+};
+
+const {destination, activity, accommodation, companion, duration, season, mustTryFood}=dreamHoliday
+
+
+console.log(`I would love to go to ${destination}, for ${activity}. I'd sleep in a ${accommodation} and hang out with my ${companion} for ${duration}.`);
+
+/* 
+Challenge
+1. Complete the object dreamHoliday with whatever
+   information is true for you. Feel free to add 
+   extra properties or change the existing ones. 
+2. Destructure the object and use the individual 
+   variables to log out one or more sentences about 
+   your dream holiday. 
+   
+E.g. "I would love to go to Austin, Texas to visit the Tesla HQ. 
+     I'd sleep in a luxury ranch and hang out with Elon Musk all day."
+*/
