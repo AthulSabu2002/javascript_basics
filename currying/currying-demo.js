3d11d215ae35a060ad1976c9b133ff755e6c0985
athulsabu <athulsabu75@gmail.com>
2025-01-01 23:26:04 +0530

    recursion & currying

---
+++ currying/currying-demo.js
@@ -0,0 +1,15 @@
+function calculateVolume(length) {
+    return function(width) {
+        return function(height) {
+            return length * width * height
+        }
+    }
+}
+
+// const withLength = calculateVolume(2)
+// const withLengthAndWidth = withLength(3)
+// const volume = withLengthAndWidth(4)
+
+const volume = calculateVolume(2)(3)(4)
+
+console.log(volume)
